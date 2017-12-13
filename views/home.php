c2326b650b83fd0abe44dc8d31a76bd9b02d4b66
archistico <info @archistico.com>
2017-12-13 12:13:17 +0100

    barra laterale

--- views/home.php
+++ views/home.php
@@ -17,11 +17,36 @@ Html_default::JUMBOTRON("Studio medico", "Home");
 Html_default::SHOW_NOTICES(Flashmessage::READ(Autaut::LOGGATO(), File::FILENAME(__FILE__)));
 
 ?>
-    <div class='row'>
-        <div class='col-md-12'>
-            <h1>Routing</h1>
+
+<div class="row align-items-start h-100">
+  <div class="col-8">
+    <h1>Appuntamenti</h1>
+    
+
+    <div class="row align-items-start">
+        <div class="col-12">
+            <h2>Lista</h2>
         </div>
     </div>
+
+
+  </div>
+  <div class="col-4 h-100 barralaterale">
+    <h1>Da fare</h1>
+    <div class="row align-items-start">
+        <div class="col-12">
+            <h2>Dottoressa</h2>
+        </div>
+    </div>
+    <div class="row align-items-start">
+        <div class="col-12">
+            <h2>Collaboratrice</h2>
+        </div>
+    </div>
+
+  </div>
+</div>
+
 <?php
 /* -----------------------------
  *      FINE CORPO FILE
--- views/html_default.php
+++ views/html_default.php
@@ -35,7 +35,7 @@ class Html_default {
 
     public static function OPENCONTAINER() {
         $html = "
-        <div class='container-fluid theme-showcase' role='main'>
+        <div class='container-fluid h-100 theme-showcase' role='main'>
         ";
         echo $html;
 
